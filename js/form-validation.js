9cf1e8f52534315ea41bc45167ff7639d7affd89
Michael Szeto <szetomj@gmail.com>
2016-01-30 21:42:59 +1300

    started my form validation for the form field

---
+++ js/form-validation.js
@@ -0,0 +1,13 @@
+
+function formValidation() {
+
+var emailInput = document.getElementById('email').value;
+var emailInputError = document.getElementById('email-error');
+
+if( emailInput.length < 2 || emailInput == ""){
+    emailInputError.style.display = 'block';
+    return false;
+}else {
+	return true;
+}
+}
