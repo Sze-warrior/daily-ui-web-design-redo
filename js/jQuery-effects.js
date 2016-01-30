c538fe50fb2ec88f34da60d54bd50f3bf2ffb56d
Michael Szeto <szetomj@gmail.com>
2016-01-30 17:14:36 +1300

    added in the Javascript/JQuery files. Will start form validation soon! =D

---
+++ js/jQuery-effects.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+    $("#header").hide().fadeIn(3000);
+
+
+	$('.btn-like').click(function(){
+
+		$('.btn-like').hide();
+
+	});
+
+	$('.btn-share').click(function(){
+
+		$('.btn-share').hide();
+
+	});
+
+});
