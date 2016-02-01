4fa855618650c0bc94cd138d41e99a3440762fa9
Michael Szeto <szetomj@gmail.com>
2016-02-01 18:15:26 +1300

    added the CSS transition to the How it works a tags

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,8 +1,7 @@
+function formValidation() {
 
 var emailInput = document.getElementById('email').value;
 var emailInputError = document.getElementById('email-error');
-
-function formValidation() {
 
 if( emailInput.length < 2 || emailInput == ""){
     emailInputError.style.display = 'block';
